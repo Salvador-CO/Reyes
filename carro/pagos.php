11a36c7a8c7a97525cc2e4ce9ee34d4fc2807e85
Salvador-CO <salvador.camposorihuela@gmail.com>
2021-12-28 19:01:36 -0600

    pagos

--- carro/pagos.php
+++ carro/pagos.php
@@ -26,11 +26,11 @@
    
     <div class="section"   >
         <div class="container" style="color: #000; font-size: 20px;">
-             <form>
+             <form action="pagos.php" method="POST"> 
                 <div class="form-row align-items-center" >
                     <div class="col-auto">
                       <label  for="inlineFormInput">Ingresa codigo de pedido:</label>
-                      <input style="border-bottom: medium solid" type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Ingresa codigo">
+                      <input style="border-bottom: medium solid" type="text" class="form-control mb-2" id="codigo" name="codigo" placeholder="Ingresa codigo">
                     </div>
                      
                     <div class="col-auto" style="margin-top: 45px;">
@@ -39,15 +39,83 @@
                     </div>
                 </div>  
             </form>
-                  
-             
-     
+            <?php 
+        if (empty($_POST['codigo'])) {
+            
+        }else{
+            $codigo=$_POST['codigo'];
+            $query = mysqli_query($conexion, "SELECT * FROM `pedidos` WHERE `clave`='$codigo'  ");
+            $dato = mysqli_fetch_array($query);
+            $numero = mysqli_num_rows($query);
+            if ($numero>=1) {
+            ?>
+
+            <div class="card shadow mb-4" >
+                 
+                <div class="card-body">
+                    <div class="table-responsive" id="tabla">
+                        <?php
+                        $sql1 ="SELECT Nombre, productos, total, pago FROM `pedidos` WHERE `clave`='$codigo' ";
+                        $consultap = mysqli_query($conexion, $sql1);
+                        if($consultap->num_rows === 0) {
+                        ?>
+                        <p>No has agregado ningun producto</p>
+                        <?php
+                        } else {
+                        ?>
+                        <table class="table table-bordered"   width="100%" cellspacing="0">
+                            <thead class="thead-dark">
+                                <tr>
+                                     
+                                    <th scope="col">Nombre</th>
+                                    <th scope="col">Productos</th>
+                                    <th scope="col">Total</th>
+                                    <th scope="col">Estado de pago</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                 
+                                while($rowedit = mysqli_fetch_array($consultap)){
+                                $nom=$rowedit["Nombre"];
+                                $pro = $rowedit["productos"];
+                                $total = $rowedit["total"];
+                                $pagoes = $rowedit["pago"];
+                                ?>    
+                                <tr >
+                                <td><?php echo $nom; ?></td>
+                                <td><?php echo $pro; ?></td>
+                                <td>$ <?php echo $total; ?></td>
+                                <td><?php echo $pagoes; ?></td>
+  
+                                 
+                                </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                         
+                        <?php }?> <!-- fin del else -->
+                    </div>
+                </div>
+            </div>
+            <center><h1>Cargar tiket de pago</h1></center>
+
+            <?php
+            }else{
+                echo "<br><center> <font size='10' color='red'> no hay pedido con ese codigo </font></center>";
+            }
+        }
+
+         ?>
         </div>
+
+
     </div>
    
 
     <!-- seccion clara -->
     
     <!-- fin seccion clara -->
+     
 
     <?php include_once "include/footer.php"; ?>
